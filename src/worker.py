5b69e6e02320299a5d39862404f6006e8da0edc1
dangnm-2032 <dangnm.working@gmail.com>
2025-10-10 13:44:37 +0700

    fix: regex

--- src/worker.py
+++ src/worker.py
@@ -148,7 +148,7 @@ def worker():
                 message_content = messages[0].content[0].text
 
                 reply = message_content.value
-                reply = re.sub(r"【\S*】", " ", reply)
+                reply = re.sub(r"【.*?】", " ", reply)
 
                 # Normalize spaces (avoid double spaces after removal)
                 reply = re.sub(r"\s{2,}", " ", reply).strip()
